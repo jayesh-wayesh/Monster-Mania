32ca8f0bbbaf9b8551fd19d47134bbf8a8433fa4
Jayesh Bhalla <jayeshbhalla891@gmail.com>
2021-06-28 01:22:03 +0530

    Updated game header

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <>
         <h1>Monster Mania</h1>
-        <p className="container-desc">Monster Mania is a collection game in which one can win a random Monster NFT daily! There are a total of 10 different monsters. Once you have completed a collection, you can exchange it for a <span>King Monster NFT</span> and rule the monster world.</p>
+        <p className="container-desc">Monster Mania is a collection game in which one can win a random Monster NFT daily! There are a total of 10 different monsters. Goal is to collect all the 10 monsters. Once you have completed the collection, you'll won a <span>King Monster NFT</span> to rule the monster world.</p>
         <br/>
     </>
   )
