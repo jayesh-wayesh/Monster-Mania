7678a086b52c4615d5191c4315853c3d09364b42
Jayesh Bhalla <jayeshbhalla891@gmail.com>
2021-07-09 04:13:23 +0530

    Update create-user.js

--- src/hooks/create-user.js
+++ src/hooks/create-user.js
@@ -2,62 +2,99 @@ import React, { useState } from "react";
 import axios from 'axios';
 import Button from '@material-ui/core/Button'
 
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+        Text: 'center'
+      },
+    },
+  }));
 
 export default function CreateUser(props) {
 
-	const [userInput, setUserInput] = useState('')
-	
-	const onChangeUser = (e) => {
-		setUserInput(e.target.value)
-	}
-	
-	const onSubmit = async (e) => {
-		e.preventDefault();
-		
-		const username = userInput;
-		const newUser = {
-			username: username, //<string>
-		}
+    const [accountUsername, setAccountUsername] = useState('')
+    const [accountPassword, setAccountPassword] = useState('')
+    const classes = useStyles()
+        
+    const onChangeUser = (e) => {
+        setAccountUsername(e.target.value)
+    }
+        
+    const onChangePassword = (e) => {
+        setAccountPassword(e.target.value)
+    }
 
-		await axios.get('http://localhost:5000/users/')
+    const onSubmit = async (e) => {
+        e.preventDefault();
+                
+        const username = accountUsername;
+        const newUser = {
+            username: username, 
+            password: accountPassword,
+        }
+
+        await axios.put('http://localhost:5000/users/' + username + '/authenticate', {password: accountPassword})
             .then(res => {  
-                var userExist = res.data.find(user => user === username)
-                return userExist
+                var authenticate = res.data.authenticate
+                return authenticate
             })
-            .then(async (userExist) => {
-                if(userExist === undefined) {
-                    await axios.post('http://localhost:5000/users/add', newUser)
-                        .then(res => console.log(res.data))
-                }else{
-                    props.setOldUser(true)
-                }
+            .then(async (authenticate) => {
 
-                props.setUsername(username)
-                localStorage.setItem("username", username)
+                console.log('authenticate : ', authenticate)
+
+                // if(authenticate === false){
+                //     alert('⚠️ Password Incorrect')
+                // }else{
+                //     // Create new user
+                //     if(authenticate === undefined){
+                //         await axios.post('http://localhost:5000/users/add', newUser)
+                //             .then(res => console.log(res.data))
+                //     }else{
+                //         // old user
+                //         props.setOldUser(true)
+                //     }
+                //     props.setUsername(username)
+                //     localStorage.setItem("username", username)
+                // }
             })
             .catch(err => console.log('⚠️ Error : ' + err))
-	}
+    }
 
-	return (
-		<section className="section">
-			<h2>Sign up : </h2>
-			<br/>
-			<form onSubmit={onSubmit}>
-				<div className="form-group">
-				<label>Username : </label>
-				<input 
-					type="text" 
-					className="form-control"
-					value={userInput}
-					onChange={onChangeUser}
-				/>
-				</div>
-				<br/>
-				<div className="form-group">
-				<Button className="login" type="submit" size="small" variant="outlined">
-					Start game
-				</Button>
-				</div>
+    return (
+        <section className="section">
+            <h2>Sign up : </h2>
+            <br/>
+            <form className={classes.root} noValidate autoComplete="off">
+                <div>
+                    <TextField 
+                        id="outlined-basic"
+                        label="Username"
+                        variant="outlined"
+                        className="form-control x"
+                        value={accountUsername}
+                        onChange={onChangeUser}
+                    />
+                </div>
+                <div>
+                    <TextField 
+                        id="outlined-basic"
+                        label="Password"
+                        variant="outlined"
+                        className="form-control x"
+                        value={accountPassword}
+                        onChange={onChangePassword}
+                    />
+                </div>
+                <div>
+                    <Button className="login" type="submit" size="small" variant="outlined">
+                        Start game
+                    </Button>
+                </div>
 			</form>
 		</section>
 	)
