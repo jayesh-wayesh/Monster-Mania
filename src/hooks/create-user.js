64e64839d5a23ef73fce605ac1b3ca6503b57e44
Jayesh Bhalla <jayeshbhalla891@gmail.com>
2021-07-10 02:06:21 +0530

    updated signup page

--- src/hooks/create-user.js
+++ src/hooks/create-user.js
@@ -1,25 +1,13 @@
-import React, { useState } from "react";
-import axios from 'axios';
+import React, { useState } from "react"
+import axios from 'axios'
 import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-        width: '25ch',
-        Text: 'center'
-      },
-    },
-  }));
 
 export default function CreateUser(props) {
 
     const [accountUsername, setAccountUsername] = useState('')
     const [accountPassword, setAccountPassword] = useState('')
-    const classes = useStyles()
         
     const onChangeUser = (e) => {
         setAccountUsername(e.target.value)
@@ -29,7 +17,7 @@ export default function CreateUser(props) {
         setAccountPassword(e.target.value)
     }
 
-    const onSubmit = async (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
                 
         const username = accountUsername;
@@ -47,51 +35,56 @@ export default function CreateUser(props) {
 
                 console.log('authenticate : ', authenticate)
 
-                // if(authenticate === false){
-                //     alert('⚠️ Password Incorrect')
-                // }else{
-                //     // Create new user
-                //     if(authenticate === undefined){
-                //         await axios.post('http://localhost:5000/users/add', newUser)
-                //             .then(res => console.log(res.data))
-                //     }else{
-                //         // old user
-                //         props.setOldUser(true)
-                //     }
-                //     props.setUsername(username)
-                //     localStorage.setItem("username", username)
-                // }
+                if(authenticate === false){
+                    alert('⚠️ Password Incorrect')
+                }else{
+                    // Create new user
+                    if(authenticate === undefined){
+                        await axios.post('http://localhost:5000/users/add', newUser)
+                            .then(res => console.log(res.data))
+                    }else{
+                        // old user
+                        props.setOldUser(true)
+                    }
+                    props.setUsername(username)
+                    localStorage.setItem("username", username)
+                }
             })
             .catch(err => console.log('⚠️ Error : ' + err))
     }
 
     return (
         <section className="section">
-            <h2>Sign up : </h2>
-            <br/>
-            <form className={classes.root} noValidate autoComplete="off">
-                <div>
+            <h2>Sign up: </h2>
+            <form className="form-group" noValidate autoComplete="off">
+                <div className="form-input">
                     <TextField 
                         id="outlined-basic"
                         label="Username"
                         variant="outlined"
-                        className="form-control x"
+                        className="form-control"
                         value={accountUsername}
                         onChange={onChangeUser}
                     />
                 </div>
-                <div>
+                <div className="form-input"> 
                     <TextField 
                         id="outlined-basic"
                         label="Password"
                         variant="outlined"
-                        className="form-control x"
+                        className="form-control"
                         value={accountPassword}
                         onChange={onChangePassword}
                     />
                 </div>
                 <div>
-                    <Button className="login" type="submit" size="small" variant="outlined">
+                    <Button 
+                        className="login" 
+                        type="submit" 
+                        size="small" 
+                        variant="outlined" 
+                        onClick={handleSubmit}
+                    >
                         Start game
                     </Button>
                 </div>
